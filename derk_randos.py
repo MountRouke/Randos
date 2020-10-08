19b8e661d30a19d0392674cbadf2e08d0fee61e6
Fredrik Norén <fredrik.jw.noren@gmail.com>
2020-10-09 01:14:08 +0200

    Bump derk

--- derk_randos.py
+++ derk_randos.py
@@ -1,30 +1,27 @@
-from gym_derk.envs import DerkEnv, ConnectionLostError
+from gym_derk import DerkSession, DerkAgentServer, DerkAppInstance
+import asyncio
 from argparse import ArgumentParser
 import logging
 logging.basicConfig(level = logging.INFO)
 
-def main(env_args={ "mode": "server" }):
-  env = DerkEnv(**env_args)
-
+async def run_session(env: DerkSession):
   while True:
-    observation_n = env.reset()
-    while True:
+    await env.reset()
+    while not env.done:
       action_n = [env.action_space.sample() for i in range(env.n_agents)]
-      observation_n, reward_n, done_n, info = env.step(action_n)
-      if all(done_n):
-        print("Episode finished")
-        break
-  env.close()
+      await env.step(action_n)
 
 if __name__ == "__main__":
   parser = ArgumentParser()
   parser.add_argument("-s", "--server", action="store_true", dest="server", default=False)
   parser.add_argument("-c", "--connected", action="store_true", dest="connected", default=False)
   args = parser.parse_args()
-  while True:
-    try: main({ 'mode': "server" if args.server else ("connected" if args.connected else None) })
-    except ConnectionLostError as err:
-      print('Connection lost')
-      err.env.close()
-    if not args.server:
-      break
+  server = DerkAgentServer(run_session, port=8789 if args.server else 8788)
+  asyncio.get_event_loop().run_until_complete(server.start())
+  if args.server:
+    asyncio.get_event_loop().run_forever()
+  else:
+    instance = DerkAppInstance(browser_logs=True)
+    asyncio.get_event_loop().run_until_complete(
+      instance.run_session(agent_hosts='dual_local' if args.connected else 'single_local')
+    )
